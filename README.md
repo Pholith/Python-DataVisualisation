33c649bc1300bbf5ad39221e5b30475455558bd3
Pholith <vincent22062@gmail.com>
2020-11-14 19:56:45 +0100

    nettoyage des fichiers + début readme + modifications html

---
+++ README.md
@@ -0,0 +1,23 @@
+La version de python recommandé est 3.7, car la librairie dash ne supporte pas encore les versions supérieurs.
+
+Commandes d'installation: 
+python3 -m pip install pandas
+python3 -m pip install simpledbf
+python3 -m pip install numpy==1.19.3
+python3 -m pip install dash
+
+Commande d'exécution:
+python3 __init__.py
+
+
+Copié collé blackboard:
+
+Les représentations graphiques incluses dans le dashboard devront éclairer le sujet que vous avez choisi, en répondant à une problématique. 
+
+    un rapport d'analyse qui met en avant les principales conclusions extraites des données
+    un User Guide qui permet de déployer et d'utiliser votre dashboard
+    un Developper Guide qui permet de comprendre l'architecture du code et de modifier ou d'étendre celui ci
+
+Votre travail sera déployé dans un contexte différent de celui de votre machine de développement. Toutes les instructions devront être décrites dans le User Guide.
+Le dashboard contiendra a minima un histogramme et une représentation géolocalisée.
+Votre code doit être structuré et documenté. Une attention particulière sera portée au choix des structures de données et aux fonctions/classes utilisées. On privilégiera les "bonnes pratiques" de Python.
--- __init__.py
+++ __init__.py
@@ -1,95 +1,92 @@
-import pandas as pd
-import typing
-import plotly.express as px
-#import numpy as np
-from simpledbf import Dbf5
+import typing # Enable static variable annotations
+from simpledbf import Dbf5 # Used to extract data from .dbf files
+import os
+dirname = os.path.dirname(__file__)
+
+# DataScience and display libs
 from urllib.request import urlopen
 import json
+import plotly.express as px
+import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+
 def file_len(fname):
+	"""Get the file len. Was usefull for tests on big documents
+
+	Args:
+		fname (string): path of the file
+
+	Returns:
+		int: number of lines
+	"""
 	with open(fname, encoding="utf-8") as f:
 		for i, l in enumerate(f):
 			pass
 	return i + 1
-	
-def copyStartFile():
-	i = 0
-	with open("mv_immeubles_2020t2.csv","r", encoding="utf-8") as f:
-		with open("test.csv", "w", encoding="utf-8") as f2:
-			for line in f.readlines():
-				f2.write(line);
-				i += 1;
-				if (i > 5000): 
-					break
 
 
-#copyStartFile()
+def main():
+		
+	dbf = Dbf5("2020T2_communes.dbf")
 
-dbf = Dbf5('2020T2_communes.dbf')
-#dbf = Dbf5('2020_T2_departement.dbf')
+	df = dbf.to_dataframe()
 
-df = dbf.to_dataframe()
-#iris = pd.read_csv("mv_immeubles_2020t2.csv")
-#iris = pd.read_csv("test.csv")
+	# Source: https://geo.api.gouv.fr/communes?fields=nom,code&format=json&geometry=centre
+	# Read the data
+	with urlopen("file:" + os.path.join(dirname, "communes-20190101.json")) as response:
+		counties = json.load(response)
 
-#https://geo.api.gouv.fr/communes?fields=nom,code&format=json&geometry=centre
-with urlopen('file:///I:\devoirsjuju\Esiee1\Python\Python-DataVisualisation/communes-20190101_cut.json') as response:
-#with urlopen('file:///C:/Users/vince/OneDrive/Documents/Projet /Python-DataVisualisation/communes-20190101.json') as response:
-	counties = json.load(response)
+	# Debug help
+	for col in df.columns:
+		print(col, end=" ")
+	print("")
+
+	# Create a map on the fiber cover
+	fig = px.choropleth_mapbox(
+		df,
+		geojson=counties,
+		locations="INSEE_COM",
+		featureidkey="properties.insee",
+		color="couv",
+		color_continuous_scale="Viridis",
+		range_color=(0, 80),
+		mapbox_style="carto-positron",
+		zoom=4,
+		center={"lat": 48.853, "lon": 2.3488},
+		opacity=0.2,
+		hover_data=["NOM_COM"],  # ftth
+		labels={"NOM_COM": "Commune ", "couv": "Couverture de fibre "},
+	)
+
+	fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
 
-#with urlopen('file:///C:/Users/vince/OneDrive/Documents/Projet/Python-DataVisualisation/communes-20190101.json') as response:
-#    counties = json.load(response)
-
-
-
-""""fig = px.scatter_mapbox(iris, lat="y", lon="x", 
-				color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
-				mapbox_style="carto-positron", hover_data=["pm_etat"])"""
-"""fig = px.density_mapbox(iris, lat="y", lon="x", 
-	color_continuous_scale=px.colors.cyclical.Twilight, zoom=10,
-	mapbox_style="carto-positron", hover_data=["pm_etat"], radius=5)"""
-
-
-fig = px.choropleth_mapbox(df, geojson=counties, locations='INSEE_COM', 
-					featureidkey="properties.insee", 
-					color='couv', 
-					color_continuous_scale="Viridis",
-					range_color=(0, 80),
-					mapbox_style="carto-positron",
-					zoom=4, center = {"lat": 48.853, "lon": 2.3488},
-					opacity=0.2,
-					labels={'NOM_COM':'nom'})
-
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-	
-	
-	
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash()
-server = app.server
-app.layout = html.Div(children=[html.H1(children='TEST'),
-								dcc.Graph(
-									id='example-graph-1',
-									figure=fig
-								),
-								html.Div(children=
-										'''
-										Data source from https://www.realestate.com.au/neighbourhoods @Dec 2019
-										'''
-										)
-							])
-#page.run_server(debug=True, threaded=True, use_reloader=False)
-#fig.show()
+	# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+	app = dash.Dash()
+	server = app.server
+	app.layout = html.Div(
+		children=[
+			html.H1(children="Où en est le déploiment de la fibre en France ?"),
+			html.Div(
+				children=[
+					dcc.Graph(id="example-graph-1", figure=fig, style = {"width": "100%"}),
+					dcc.Graph(id="example-graph-2", figure=fig, style = {"width": "100%"}),
+				],
+				style =	{"display": "flex", "flex-direction": "row", "width": "95%"}
+			),
+			html.Div(
+				children=["Source DataGouv ",
+					html.A("link", href="https://www.data.gouv.fr/fr/datasets/le-marche-du-haut-et-tres-haut-debit-fixe-deploiements/")
+				]
+			),
+		]
+	)
+	app.run_server(debug=True, threaded=True, use_reloader=False)
 
 
 if __name__ == "__main__":
-	
-	app.run_server(debug=True, threaded=True, use_reloader=False)
-	
-	
-	
+	main()
